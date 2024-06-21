f7cc6b82857fecc3364965c72f670eb24438dd80
LineWulff <lw@himalaya.dk>
2024-06-21 14:08:55 -0600

    updated 21/6

--- 2406_Analysis_BM-PBSvsHA107-PBS-8wk.R
+++ 2406_Analysis_BM-PBSvsHA107-PBS-8wk.R
@@ -13,6 +13,7 @@ library(Signac)
 library(Seurat)
 library(biovizBase)
 library(EnsDb.Mmusculus.v79)
+library(GenomicRanges) 
 
 #### ---- variables used throughout script ---- ####
 rm(list = ls())
@@ -26,10 +27,59 @@ project <- "BM-PBSvsHA107-PBS-8wk"
 BMPBSPBS8wk <- readRDS("/Volumes/Promise RAID/Line/projects/24_TI_IgnacioWulff/samples/BM-PBS-PBS-8wk/BM-PBS-PBS-8wk.rds")
 BMHAPBS8wk <- readRDS("/Volumes/Promise RAID/Line/projects/24_TI_IgnacioWulff/samples/BM-HA107-PBS-8wk/BM-HA107-PBS-8wk.rds")
 
-## Identify common peaks
-rownames(BMPBSPBS8wk@assays$peaks)[rownames(BMPBSPBS8wk@assays$peaks) %in% rownames(BMHAPBS8wk@assays$peaks)] # only 178?
+## Create common peak sets and subset the object assays based on this set
+# read in peak sets
+peaks.HA <- read.table(
+  file = "/Volumes/Promise RAID/Line/projects/24_TI_IgnacioWulff/samples/BM-HA107-PBS-8wk/peaks.bed",
+  col.names = c("chr", "start", "end")
+)
+peaks.PBS <- read.table(
+  file = "/Volumes/Promise RAID/Line/projects/24_TI_IgnacioWulff/samples/BM-PBS-PBS-8wk/peaks.bed",
+  col.names = c("chr", "start", "end")
+)
 
-# try anyway
+# convert to genomic ranges
+gr.HA <- makeGRangesFromDataFrame(peaks.HA)
+gr.PBS <- makeGRangesFromDataFrame(peaks.PBS)
+
+# Create a unified set of peaks to quantify in each dataset
+combined.peaks <- reduce(x = c(gr.HA, gr.PBS))
+
+# Filter out bad peaks based on length
+peakwidths <- width(combined.peaks)
+combined.peaks <- combined.peaks[peakwidths  < 10000 & peakwidths > 20]
+combined.peaks
+
+# make samples from existing data with these peaks
+frags.PBS <- CreateFragmentObject(
+  path = "/Volumes/Promise RAID/Line/projects/24_TI_IgnacioWulff/samples/BM-PBS-PBS-8wk/fragments.tsv.gz",
+  cells = Cells(BMPBSPBS8wk)
+)
+frags.HA <- CreateFragmentObject(
+  path = "/Volumes/Promise RAID/Line/projects/24_TI_IgnacioWulff/samples/BM-HA107-PBS-8wk/fragments.tsv.gz",
+  cells = Cells(BMHAPBS8wk)
+)
+
+PBS.counts <- FeatureMatrix(
+  fragments = frags.PBS,
+  features = combined.peaks,
+  cells = Cells(BMPBSPBS8wk)
+)
+
+HA.counts <- FeatureMatrix(
+  fragments = frags.HA,
+  features = combined.peaks,
+  cells = Cells(BMHAPBS8wk)
+)
+
+PBS_assay <- CreateChromatinAssay(PBS.counts, fragments = frags.PBS)
+BMPBSPBS8wk <- CreateSeuratObject(PBS_assay, assay = "ATAC", meta.data=BMPBSPBS8wk@meta.data)
+
+HA_assay <- CreateChromatinAssay(HA.counts, fragments = frags.HA)
+BMHAPBS8wk <- CreateSeuratObject(HA_assay, assay = "ATAC", meta.data=BMHAPBS8wk@meta.data)
+
+
+
 # merge all datasets, adding a cell ID to make sure cell names are unique
 combined <- merge(
   x = BMPBSPBS8wk,
@@ -48,5 +98,5 @@ combined <- RunTFIDF(combined)
 combined <- FindTopFeatures(combined, min.cutoff = 20)
 combined <- RunSVD(combined)
 combined <- RunUMAP(combined, dims = 2:50, reduction = 'lsi')
-DimPlot(combined, group.by = 'dataset', pt.size = 0.1)
+DimPlot(combined, group.by = 'orig.ident', pt.size = 0.1)
 
