7a2bca6929f606c9c81b5aa0da39e38192175395
LineWulff <lw@himalaya.dk>
2025-07-22 08:26:59 -0600

    All DAR added

--- 25_07_BM-PBSHA107LPSLPS-82k_DiffPeakAnalysis.R
+++ 25_07_BM-PBSHA107LPSLPS-82k_DiffPeakAnalysis.R
@@ -160,3 +160,215 @@ names(DA_peaks_conditions) <- paste("cluster", names(DA_peaks_conditions), sep =
 ## save in excel format
 write.xlsx(DA_peaks_conditions, file = paste(outdir, dato, "_LSKMonoNeu_IDlabs_PerClusCompvsCond_HA107LPSvsPBSLPS8wk.xlsx", sep = ""),
            rowNames = T)
+
+#### ---- PBS-PBS-8wk vs PBS-LPS-8wk ---- ####
+DA_peaks_conditions <- list()
+Idents(combined) <- "orig.ident"
+
+for (clus in unique(combined@meta.data$ID_labs)){
+  DA_peaks_IDlabs <- FindMarkers(
+    object = subset(combined, cells = rownames(combined@meta.data[combined@meta.data$ID_labs==clus,])),
+    only.pos = FALSE,
+    ident.1 = 'BM-PBS-PBS-8wk', 
+    ident.2 = 'BM-PBS-LPS-8wk',
+    test.use = 'LR',
+    latent.vars = 'nCount_peaks',
+    logfc.threshold = 0)
+  # add ass. genename
+  open_regs <- rownames(DA_peaks_IDlabs)
+  closest_genes <- ClosestFeature(subset(combined, cells = rownames(combined@meta.data[combined@meta.data$ID_labs==clus,])),
+                                  regions = open_regs, annotation = peakAnno.edb@anno)
+  DA_peaks_IDlabs$gene_name <-NA
+  DA_peaks_IDlabs[rownames(DA_peaks_IDlabs) %in% closest_genes$query_region,]$gene_name <- closest_genes$gene_name
+  
+  # Add sign. groups (mainly for plotting), all non sign. will be removed before saving
+  # beacuse not all have significant values, should include option of none
+  DA_peaks_IDlabs$sign <- "not sign."
+  if (any(DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05)){
+    DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05,]$sign <- "BM-PBS-PBS-8wk"}
+  if (any(DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05)){
+    DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05,]$sign <- "BM-PBS-LPS-8wk"}
+  
+  # plot as volcano
+  volc_plot <- ggplot(DA_peaks_IDlabs, aes(x = avg_log2FC, y = -log10(p_val_adj), colour = sign ))+
+    geom_point_rast()+
+    geom_vline(xintercept = c(-0.25,0.25), linetype = "dashed")+ # sign. threshold
+    geom_hline(yintercept = c(-log10(0.05)), linetype = "dashed")+ # sign. threshold
+    geom_vline(xintercept = c(0))+ #0
+    scale_color_manual(values = c('BM-PBS-PBS-8wk'="#F8766D",'BM-PBS-LPS-8wk'="#00BFC4", 'not sign.'='lightgrey'))+
+    theme_classic()+
+    ylab("-log10(adj. p-value)")+xlab("avg. log2FC")+
+    guides(colour=guide_legend(title="Significance"))
+  pdf(paste0(outdir,dato,project,"VolcanoPlotDAR_cluster",clus,"PBSPBSvsPBSLPS.pdf"),height = 4, width = 5.5)
+  print(volc_plot)
+  dev.off()
+  # subset to only include significant values, logFC 0.5 and p.adj 0.05
+  DA_peaks_IDlabs <- DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05 |
+                                       DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05,]
+  DA_peaks_conditions[[clus]] <- DA_peaks_IDlabs 
+}
+
+names(DA_peaks_conditions) <- paste("cluster", names(DA_peaks_conditions), sep = "_")
+
+## save in excel format
+write.xlsx(DA_peaks_conditions, file = paste(outdir, dato, "_LSKMonoNeu_IDlabs_PerClusCompvsCond_PBSPBSvsPBSLPS8wk.xlsx", sep = ""),
+           rowNames = T)
+
+#### ---- PBS-PBS-21d vs HA107-PBS-21d ---- ####
+DA_peaks_conditions <- list()
+Idents(combined) <- "orig.ident"
+
+for (clus in unique(combined@meta.data$ID_labs)){
+  DA_peaks_IDlabs <- FindMarkers(
+    object = subset(combined, cells = rownames(combined@meta.data[combined@meta.data$ID_labs==clus,])),
+    only.pos = FALSE,
+    ident.1 = 'BM-PBS-PBS-21d', 
+    ident.2 = 'BM-HA107-PBS-21d',
+    test.use = 'LR',
+    latent.vars = 'nCount_peaks',
+    logfc.threshold = 0)
+  # add ass. genename
+  open_regs <- rownames(DA_peaks_IDlabs)
+  closest_genes <- ClosestFeature(subset(combined, cells = rownames(combined@meta.data[combined@meta.data$ID_labs==clus,])),
+                                  regions = open_regs, annotation = peakAnno.edb@anno)
+  DA_peaks_IDlabs$gene_name <-NA
+  DA_peaks_IDlabs[rownames(DA_peaks_IDlabs) %in% closest_genes$query_region,]$gene_name <- closest_genes$gene_name
+  
+  # Add sign. groups (mainly for plotting), all non sign. will be removed before saving
+  # beacuse not all have significant values, should include option of none
+  DA_peaks_IDlabs$sign <- "not sign."
+  if (any(DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05)){
+    DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05,]$sign <- "BM-PBS-PBS-21d"}
+  if (any(DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05)){
+    DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05,]$sign <- "BM-HA107-PBS-21d"}
+  
+  # plot as volcano
+  volc_plot <- ggplot(DA_peaks_IDlabs, aes(x = avg_log2FC, y = -log10(p_val_adj), colour = sign ))+
+    geom_point_rast()+
+    geom_vline(xintercept = c(-0.25,0.25), linetype = "dashed")+ # sign. threshold
+    geom_hline(yintercept = c(-log10(0.05)), linetype = "dashed")+ # sign. threshold
+    geom_vline(xintercept = c(0))+ #0
+    scale_color_manual(values = c('BM-PBS-PBS-21d'="#F8766D",'BM-HA107-PBS-21d'="#00BFC4", 'not sign.'='lightgrey'))+
+    theme_classic()+
+    ylab("-log10(adj. p-value)")+xlab("avg. log2FC")+
+    guides(colour=guide_legend(title="Significance"))
+  pdf(paste0(outdir,dato,project,"VolcanoPlotDAR_cluster",clus,"PBSvsHA107_21d.pdf"),height = 4, width = 5.5)
+  print(volc_plot)
+  dev.off()
+  # subset to only include significant values, logFC 0.5 and p.adj 0.05
+  DA_peaks_IDlabs <- DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05 |
+                                       DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05,]
+  DA_peaks_conditions[[clus]] <- DA_peaks_IDlabs 
+}
+
+names(DA_peaks_conditions) <- paste("cluster", names(DA_peaks_conditions), sep = "_")
+
+## save in excel format
+write.xlsx(DA_peaks_conditions, file = paste(outdir, dato, "_LSKMonoNeu_IDlabs_PerClusCompvsCond_PBSHA107PBSPBS21d.xlsx", sep = ""),
+           rowNames = T)
+
+#### ---- PBS-LPS-21d vs HA107-LPS-21d ---- ####
+DA_peaks_conditions <- list()
+Idents(combined) <- "orig.ident"
+
+for (clus in unique(combined@meta.data$ID_labs)){
+  DA_peaks_IDlabs <- FindMarkers(
+    object = subset(combined, cells = rownames(combined@meta.data[combined@meta.data$ID_labs==clus,])),
+    only.pos = FALSE,
+    ident.1 = 'BM-PBS-LPS-21d', 
+    ident.2 = 'BM-HA107-LPS-21d',
+    test.use = 'LR',
+    latent.vars = 'nCount_peaks',
+    logfc.threshold = 0)
+  # add ass. genename
+  open_regs <- rownames(DA_peaks_IDlabs)
+  closest_genes <- ClosestFeature(subset(combined, cells = rownames(combined@meta.data[combined@meta.data$ID_labs==clus,])),
+                                  regions = open_regs, annotation = peakAnno.edb@anno)
+  DA_peaks_IDlabs$gene_name <-NA
+  DA_peaks_IDlabs[rownames(DA_peaks_IDlabs) %in% closest_genes$query_region,]$gene_name <- closest_genes$gene_name
+  
+  # Add sign. groups (mainly for plotting), all non sign. will be removed before saving
+  # beacuse not all have significant values, should include option of none
+  DA_peaks_IDlabs$sign <- "not sign."
+  if (any(DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05)){
+    DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05,]$sign <- "BM-PBS-LPS-21d"}
+  if (any(DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05)){
+    DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05,]$sign <- "BM-HA107-LPS-21d"}
+  
+  # plot as volcano
+  volc_plot <- ggplot(DA_peaks_IDlabs, aes(x = avg_log2FC, y = -log10(p_val_adj), colour = sign ))+
+    geom_point_rast()+
+    geom_vline(xintercept = c(-0.25,0.25), linetype = "dashed")+ # sign. threshold
+    geom_hline(yintercept = c(-log10(0.05)), linetype = "dashed")+ # sign. threshold
+    geom_vline(xintercept = c(0))+ #0
+    scale_color_manual(values = c('BM-PBS-LPS-21d'="#F8766D",'BM-HA107-LPS-21d'="#00BFC4", 'not sign.'='lightgrey'))+
+    theme_classic()+
+    ylab("-log10(adj. p-value)")+xlab("avg. log2FC")+
+    guides(colour=guide_legend(title="Significance"))
+  pdf(paste0(outdir,dato,project,"VolcanoPlotDAR_cluster",clus,"PBSvsHA107LPS_21d.pdf"),height = 4, width = 5.5)
+  print(volc_plot)
+  dev.off()
+  # subset to only include significant values, logFC 0.5 and p.adj 0.05
+  DA_peaks_IDlabs <- DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05 |
+                                       DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05,]
+  DA_peaks_conditions[[clus]] <- DA_peaks_IDlabs 
+}
+
+names(DA_peaks_conditions) <- paste("cluster", names(DA_peaks_conditions), sep = "_")
+
+## save in excel format
+write.xlsx(DA_peaks_conditions, file = paste(outdir, dato, "_LSKMonoNeu_IDlabs_PerClusCompvsCond_PBSHA107LPSLPS21d.xlsx", sep = ""),
+           rowNames = T)
+
+#### ---- PBS-PBS-21d vs PBS-LPS-21d ---- ####
+DA_peaks_conditions <- list()
+Idents(combined) <- "orig.ident"
+
+for (clus in unique(combined@meta.data$ID_labs)){
+  DA_peaks_IDlabs <- FindMarkers(
+    object = subset(combined, cells = rownames(combined@meta.data[combined@meta.data$ID_labs==clus,])),
+    only.pos = FALSE,
+    ident.1 = 'BM-PBS-PBS-21d', 
+    ident.2 = 'BM-PBS-LPS-21d',
+    test.use = 'LR',
+    latent.vars = 'nCount_peaks',
+    logfc.threshold = 0)
+  # add ass. genename
+  open_regs <- rownames(DA_peaks_IDlabs)
+  closest_genes <- ClosestFeature(subset(combined, cells = rownames(combined@meta.data[combined@meta.data$ID_labs==clus,])),
+                                  regions = open_regs, annotation = peakAnno.edb@anno)
+  DA_peaks_IDlabs$gene_name <-NA
+  DA_peaks_IDlabs[rownames(DA_peaks_IDlabs) %in% closest_genes$query_region,]$gene_name <- closest_genes$gene_name
+  
+  # Add sign. groups (mainly for plotting), all non sign. will be removed before saving
+  # beacuse not all have significant values, should include option of none
+  DA_peaks_IDlabs$sign <- "not sign."
+  if (any(DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05)){
+    DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05,]$sign <- "BM-PBS-PBS-21d"}
+  if (any(DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05)){
+    DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05,]$sign <- "BM-PBS-LPS-21d"}
+  
+  # plot as volcano
+  volc_plot <- ggplot(DA_peaks_IDlabs, aes(x = avg_log2FC, y = -log10(p_val_adj), colour = sign ))+
+    geom_point_rast()+
+    geom_vline(xintercept = c(-0.25,0.25), linetype = "dashed")+ # sign. threshold
+    geom_hline(yintercept = c(-log10(0.05)), linetype = "dashed")+ # sign. threshold
+    geom_vline(xintercept = c(0))+ #0
+    scale_color_manual(values = c('BM-PBS-PBS-21d'="#F8766D",'BM-PBS-LPS-21d'="#00BFC4", 'not sign.'='lightgrey'))+
+    theme_classic()+
+    ylab("-log10(adj. p-value)")+xlab("avg. log2FC")+
+    guides(colour=guide_legend(title="Significance"))
+  pdf(paste0(outdir,dato,project,"VolcanoPlotDAR_cluster",clus,"PBSvsLPS_21d.pdf"),height = 4, width = 5.5)
+  print(volc_plot)
+  dev.off()
+  # subset to only include significant values, logFC 0.5 and p.adj 0.05
+  DA_peaks_IDlabs <- DA_peaks_IDlabs[DA_peaks_IDlabs$avg_log2FC>0.25 & DA_peaks_IDlabs$p_val_adj<0.05 |
+                                       DA_peaks_IDlabs$avg_log2FC<(-0.25) & DA_peaks_IDlabs$p_val_adj<0.05,]
+  DA_peaks_conditions[[clus]] <- DA_peaks_IDlabs 
+}
+
+names(DA_peaks_conditions) <- paste("cluster", names(DA_peaks_conditions), sep = "_")
+
+## save in excel format
+write.xlsx(DA_peaks_conditions, file = paste(outdir, dato, "_LSKMonoNeu_IDlabs_PerClusCompvsCond_PBSPBSPBSLPS21d.xlsx", sep = ""),
+           rowNames = T)
